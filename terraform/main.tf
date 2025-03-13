d26f8ecf07b39480df1e7219c1ba29557548ce45
Khoa Hoang <khoahoang1698@gmail.com>
2025-03-13 11:03:51 -0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,50 @@
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
 
-resource "aws_wafv2_web_acl" "blog_web_acl" {
-  provider = aws.us_east_1
-  name = "blog-web-acl"
-  description = "Web ACL for my blog"
-  scope = "CLOUDFRONT"
+
+resource "aws_wafv2_web_acl" "example" {
+  name        = "rate-based-example"
+  description = "Example of a Cloudfront rate based statement."
+  scope       = "CLOUDFRONT"
 
   default_action {
     allow {}
   }
 
   rule {
-    name = "ip-reputation"
+    name     = "rule-1"
     priority = 1
-
-    statement {
-      managed_rule_group_statement {
-        name = "AWSManagedRulesIpReputationList"
-        vendor_name = "AWS"
-      }
-    }
 
     action {
       block {}
     }
 
+    statement {
+      rate_based_statement {
+        limit              = 10000
+        aggregate_key_type = "IP"
+
+        scope_down_statement {
+          geo_match_statement {
+            country_codes = ["US", "NL"]
+          }
+        }
+      }
+    }
+
     visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name = "ip-reputation-rule-blog"
-      sampled_requests_enabled = true
+      cloudwatch_metrics_enabled = false
+      metric_name                = "friendly-rule-metric-name"
+      sampled_requests_enabled   = false
     }
   }
 
+  tags = {
+    Tag1 = "Value1"
+    Tag2 = "Value2"
+  }
+
   visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name = "blog-web-acl"
-    sampled_requests_enabled = true
+    cloudwatch_metrics_enabled = false
+    metric_name                = "friendly-metric-name"
+    sampled_requests_enabled   = false
   }
 }
